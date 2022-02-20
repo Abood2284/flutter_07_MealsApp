46b752fdf38031b8a8a5b86af08e70e86ba04e53
abood <sayyedabood69@gmail.com>
2022-02-20 19:47:18 +0530

    initial commit

---
+++ meals_app/lib/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Daily Meal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          // * If deivce widith is 300 only 1 child wil be there in col, if device width 500 then 2
+          maxCrossAxisExtent: 200,
+          // * Define how items shoule be sized based on there height & width
+          childAspectRatio: 3 / 2,
+          //* Spacing in col & row between each child
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+      ),
+    );
+  }
+}
