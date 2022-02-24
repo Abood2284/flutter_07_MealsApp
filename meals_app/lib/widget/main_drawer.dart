031be27ccdbc9e5adcc786a139c9c97861dec3bf
abood <sayyedabood69@gmail.com>
2022-02-24 19:18:54 +0530

    holds the logic and design of the drawer

---
+++ meals_app/lib/widget/main_drawer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../screens/filltered_screen.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon, VoidCallback tapHandler) {
+    return ListTile(
+      leading: Icon(icon, size: 26),
+      title: Text(title,
+          style: const TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          )),
+      onTap: tapHandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text('Cooking Up!',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                )),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          buildListTile('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListTile('Filters', Icons.settings, () {
+            Navigator.of(context).pushReplacementNamed(FillteredScreen.routeName);
+          }),
+        ],
+      ),
+    );
+  }
+}
