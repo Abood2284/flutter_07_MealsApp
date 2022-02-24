207eb960872e990f8db999f9c32d7f77c541a661
abood <sayyedabood69@gmail.com>
2022-02-24 19:16:01 +0530

    Holds the main strucutre of the app all key logic components and screen/routes are here

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,13 +1,70 @@
 import 'package:flutter/material.dart';
 
 import './screens/tabs_screen.dart';
-import './screens/categories_screen.dart';
 import './screens/category_recipe_screen.dart';
 import './screens/meal_details_screen.dart';
+import 'screens/filltered_screen.dart';
+import './data/dummy_data.dart';
+import './models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] == true && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] == true && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] == true && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] == true && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(
+          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+        );
+      });
+    }
+  }
+
+  bool _isMealFavorite(String id) {
+    return _favoriteMeals.any((meal) => meal.id == id);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,7 +91,7 @@ class MyApp extends StatelessWidget {
       // initialRoute: '/', // default = '/'
       // * Routes to our app arug & non-arguments
       routes: {
-        '/': (ctx) => TabsScreen(),
+        '/': (ctx) => TabsScreen(_favoriteMeals),
         // PROBLEM
         //* In bigger apps if you make a typo error in passing the key properly you can get a
         // * error that will give a hard time catching it
@@ -42,13 +99,15 @@ class MyApp extends StatelessWidget {
         // * so instead, you can store route name of that class
         // * in thier own class usin {static const} with doing that you can now acces this property without
         // * instantiating the class  'categoryRecipeScreen' => CategoryRecipeScreen.routeName
-        CategoryRecipeScreen.routeName: (ctx) => CategoryRecipeScreen(),
-        MealsDetailsScreen.routeName: (ctx) => MealsDetailsScreen(),
+        CategoryRecipeScreen.routeName: (ctx) => CategoryRecipeScreen(_favoriteMeals),
+        MealsDetailsScreen.routeName: (ctx) => MealsDetailsScreen(_toggleFavorite, _isMealFavorite),
+        FillteredScreen.routeName: (ctx) => FillteredScreen(_filters, _setFilters),
       },
+
       /// * onGenerateRoute is called if you dont specify the namedRoute in your main dart
       /// * and yet still you try to navigate so instead of throwing an error flutter
       /// * will run onGenerateRoute and will return the route you specified here
-      ///  
+      ///
       /// * Also it gives us a settings which hold metadata about route we can use
       /// * .name to check if name matches any of our route so return this or else that
       // onGenerateRoute: (settings) {
@@ -62,7 +121,7 @@ class MyApp extends StatelessWidget {
       /// * onUnknownRoute is the last backup flutter will check before throwing an error
       /// * if you dont specify any route in your route section and also no route in your onGenerateRoute
       /// * then this will be your last option to save your app from error
-      /// 
+      ///
       /// * Though this is also used as your 404 page, you can create such a page so  that if you forget to add a route
       /// * instead of getting an error flutter shows user a 404 page which says hey this page is under development
       // onUnknownRoute: (settings) {
